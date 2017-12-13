76e0eeafc93e41cd9994285c8bafc019b6a2f4a3
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-12 19:20:00 -0700

    first attempt at back button?

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 export default class Student extends Component {
-    constructor() {
-      super()
-      this.state = {
-        studentInfo: {}
-      }
-    }
+  constructor() {
+    super();
 
-    componentDidMount() {
-      axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(response => {
-      console.log(response.data)
+    this.state = {
+      studentInfo: {}
+    }
+  }
+
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(response => {
       this.setState({
-          studentInfo: response.data
-        })
+        studentInfo: response.data
       })
-    }
-    
+    })
+  }
 
-    render() {
-      return (
-        <div className="box">
-          <h1>Student</h1>
-          <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
-          <h3>Grade: {this.state.studentInfo.grade}</h3>
-          <h3>Email: {this.state.studentInfo.email}</h3>
-        </div>
-      )
-    }
+  handleBackClick() {
+    alert('bingo!')
+    // return axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(response => {
+    //   console.log(response.data);
+    //   this.setState({
+    //     studentInfo: response.data
+    //   })
+    // })
+  }
+
+  render() {
+
+    return (
+      <div className='box'>
+        <h1>Student:</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <button onClick={this.handleBackClick}>Back</button>
+      </div>
+    )
+  }
 }
